8eec9ea44e522174cf8823ae6438c5667d101c48
Mxoleli.Cwati <Mxoleli.Cwati@interfront.co.za>
2020-02-26 19:51:49 +0200

    Added extent Reporting

--- src/test/java/testCases/TC_001_ValidateSite.java
+++ src/test/java/testCases/TC_001_ValidateSite.java
@@ -2,14 +2,17 @@ package testCases;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.apache.log4j.Logger;
 
 public class TC_001_ValidateSite extends BaseTest {
 
+    static Logger log = Logger.getLogger(TC_001_ValidateSite.class.getName());
     @Test
     public void ValidateSite(){
+        log.debug("Hello this is a debug message");
+        log.info("Hello this is an info message");
 
-
-        if(driver.getTitle().equals("My Store")){
+        if(driver.getTitle().equals("My Store1")){
 
            Assert.assertTrue(true);
            System.out.println("Passed!");
@@ -18,5 +21,7 @@ public class TC_001_ValidateSite extends BaseTest {
             Assert.assertTrue(false);
             System.out.println("Passed!"); //ToDo: Capture screenshot
         }
+
+        log.info("Hello test done!!");
     }
 }
